a5abc6c387e0a74682a8f76a74e4e673333e68b1
ravik-karn-tw <ravik@thoughtworks.com>
2021-08-25 10:02:49 +0530

    [Break Point 2] Adds chracterisation test for text statement

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -2,9 +2,22 @@ package com.thoughtworks.movierental;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     @Test
-    public void test() {
+    public void shouldGenerateStatement() {
+        Customer customer = new Customer("ABC");
+        customer.addRental(new Rental(new Movie("Movie-1", Movie.REGULAR), 4));
+        customer.addRental(new Rental(new Movie("Movie-2", Movie.CHILDRENS), 5));
+        customer.addRental(new Rental(new Movie("Movie-2", Movie.NEW_RELEASE), 3));
+
+        assertEquals("Rental Record for ABC\n" +
+                "\tMovie-1\t5.0\n" +
+                "\tMovie-2\t4.5\n" +
+                "\tMovie-2\t9.0\n" +
+                "Amount owed is 18.5\n" +
+                "You earned 4 frequent renter points", customer.statement());
     }
 
 }
